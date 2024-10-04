35ed70e7740bccfb924639032a7353e13293946d
André Ferreira <andreferreiramusic@gmail.com>
2024-10-04 16:09:46 +0200

    no welconme

--- src/AllTheBooks.jsx
+++ src/AllTheBooks.jsx
@@ -1,53 +1,57 @@
-import Data from './books/horror.json';
-import Container from 'react-bootstrap/esm/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import SingleBook from './SingleBook';
-import CommentArea from './CommentArea';
-import { useState } from 'react';
-import MyNav from './MyNav';
-import Welcome from './Welcome';
-import Footer from './MyFooter';
+import Data from "./books/horror.json";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import SingleBook from "./SingleBook";
+import CommentArea from "./CommentArea";
+import { useState } from "react";
+import MyNav from "./MyNav";
+import Welcome from "./Welcome";
+import Footer from "./MyFooter";
 
-export default function AllTheBooks (props) {
-    const {searchedBook,setSearchedBook} = props;
-    const [chosenBook,setChosenBook] = useState("");
-    return (
-    <>        
-    <MyNav searchedBook={searchedBook} setSearchedBook={setSearchedBook}/>
-    <Welcome/>
-    <div className='row cols-3'>
+export default function AllTheBooks(props) {
+  const { searchedBook, setSearchedBook } = props;
+  const [chosenBook, setChosenBook] = useState("");
+  return (
+    <>
+      <MyNav searchedBook={searchedBook} setSearchedBook={setSearchedBook} />
+
+      <div className="row cols-3">
         <Container>
-            <Row xs={2}>
-                <Col>
-                <Row md={2} sm={1}>
-                {
-                    Data.filter((book) => {
-                    if (searchedBook === '') {
-                        return (book)
-                    } else {return book.title.toLowerCase().includes(searchedBook.toLowerCase())}
-                }) 
-                .map((book, index) => { 
-                    return (
-                        <div className='g-4'>
-                            <Col className='h-100'>
-                                <SingleBook chosenBook={chosenBook}
-                                setChosenBook={setChosenBook}
-                                index={index} book={book}
-                                />
-                            </Col>                    
-                        </div>
-                    )})
-                }
-                </Row>
-                </Col>
-                <Row>
-                    <CommentArea show={chosenBook}/>
-                </Row>
+          <Row xs={2}>
+            <Col>
+              <Row md={2} sm={1}>
+                {Data.filter((book) => {
+                  if (searchedBook === "") {
+                    return book;
+                  } else {
+                    return book.title
+                      .toLowerCase()
+                      .includes(searchedBook.toLowerCase());
+                  }
+                }).map((book, index) => {
+                  return (
+                    <div className="g-4">
+                      <Col className="h-100">
+                        <SingleBook
+                          chosenBook={chosenBook}
+                          setChosenBook={setChosenBook}
+                          index={index}
+                          book={book}
+                        />
+                      </Col>
+                    </div>
+                  );
+                })}
+              </Row>
+            </Col>
+            <Row>
+              <CommentArea show={chosenBook} />
             </Row>
+          </Row>
         </Container>
-    </div>
-    <Footer/>
+      </div>
+      <Footer />
     </>
-    );
+  );
 }
